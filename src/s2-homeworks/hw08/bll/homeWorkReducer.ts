a6ac0b2809b09662d27cd079c73e619b4dd74e63
and-gurin <and.gurin@gmail.com>
2022-12-06 22:21:10 +0100

    feat: add homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): Array<UserType> => {
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortArr = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? sortArr : sortArr.reverse()
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(item => item.age > action.payload)
         }
         default:
             return state
