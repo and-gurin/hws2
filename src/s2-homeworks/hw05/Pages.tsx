9d9acb46c7f642c3482f951a8e0a717083e2f393
and-gurin <and.gurin@gmail.com>
2022-12-07 21:45:03 +0100

    feat: add homework 9

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route, NavLink} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -19,12 +19,12 @@ function Pages() {
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route path={'/'} />*/}
-
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                <Route path={'/'} element={<NavLink to={PATH.PRE_JUNIOR}/>}/>
-                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/> }/>
-                <Route path={PATH.JUNIOR} element={<Junior/> }/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/> }/>
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
