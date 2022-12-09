dd3a21928ef9fac4407eae761c5d7dab7703e5c7
and-gurin <and.gurin@gmail.com>
2022-12-09 23:38:48 +0100

    fix: change year format

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -34,12 +34,12 @@ function Clock() {
         setShow(false)
     }
 
-    let formatter1 = new Intl.DateTimeFormat("ru-Ru", {	year:'2-digit', month:'2-digit', day: '2-digit'});
+    let formatter1 = new Intl.DateTimeFormat("ru-Ru", {	year:'numeric', month:'2-digit', day: '2-digit'});
     const stringTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`|| <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = `${formatter1.format(date)}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    let formatter = new Intl.DateTimeFormat("en-US", {month: "long"});
+    let formatter = new Intl.DateTimeFormat("ru-Ru", {year: "2-digit",month: "long"});
 
     const stringDay = `${(()=>{return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay(   )]})()}`|| <br/> // пишут студенты
     const stringMonth = `${formatter.format(date)}` || <br/> // пишут студенты
