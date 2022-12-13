e14ef7df794e6f949ae24777a73548f9af134eea
and-gurin <and.gurin@gmail.com>
2022-12-13 21:00:58 +0100

    feat: add homework 10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,6 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.spinner}>
+    <div className={s.dot1}></div>
+    <div className={s.dot2}></div>
+</div>
