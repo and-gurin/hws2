40dcb6a6e04b5a53a278b13d40c55749ef54a565
and-gurin <and.gurin@gmail.com>
2022-12-13 21:42:56 +0100

    fix: edit const isLoading type

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {loadingAC, StateType} from './bll/loadingReducer'
+import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
@@ -15,8 +15,7 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
 
-    const loading = useSelector<AppStoreType, StateType>(state=>state.isLoading);
-    const isLoading = loading.isLoading
+    const isLoading = useSelector<AppStoreType, boolean>(state=>state.isLoading.isLoading);
     const dispatch = useDispatch();
 
 
