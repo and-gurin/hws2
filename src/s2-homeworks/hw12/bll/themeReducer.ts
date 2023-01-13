ddbfb435f3181ed578ae3c4fdff913fd4d9875e9
and-gurin <and.gurin@gmail.com>
2023-01-13 12:50:53 +0100

    fix: add homework number 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,25 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export type ActionType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+
+export type StateType = {
+    themeId: number
+}
+
+export const themeReducer = (state = initState, action: ActionType): StateType => { // fix any
     switch (action.type) {
-        // дописать
+        case 'SET_THEME_ID':
+        return {
+            ...state, themeId: action.id
+        }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ActionType => ({ type: 'SET_THEME_ID', id }) // fix any
