4b68ddfef57dc9bcaddae17e8b9ba5530542c961
and-gurin <and.gurin@gmail.com>
2023-01-14 15:53:41 +0100

    fix: add homework number 12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react'
+import React, { useEffect} from 'react'
 import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
-import {changeThemeId, StateType} from './bll/themeReducer'
+import {changeThemeId} from './bll/themeReducer'
+import {AppStoreType} from "../hw10/bll/store";
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
@@ -20,14 +21,16 @@ const themes = [
 
 const HW12 = () => {
     // взять ид темы из редакса
-    const themeId = useSelector<StateType, number>(state=>state.themeId);
+    const themeId = useSelector<AppStoreType, number>(state=>state.theme.themeId);
     const dispatch = useDispatch();
 
-    const change = (id: any) => { // дописать функцию
+    const change = (id: number) => { // дописать функцию
+        console.log(id)
         dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
+        console.log('effect')
         document.documentElement.dataset.theme = themeId + ''
     }, [themeId])
 
