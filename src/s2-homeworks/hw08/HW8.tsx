4fb78792ea88eb81b41b965bd6fd690070e078c0
and-gurin <and.gurin@gmail.com>
2023-06-09 23:53:47 +0200

    fix: delete comments from part number 8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -5,12 +5,6 @@ import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import User from './User'
 
-/*
-* 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
-* 2 - дописать компоненту User
-* 3 - сделать стили в соответствии с дизайном
-* */
-
 export type UserType = {
     _id: number
     name: string
@@ -18,13 +12,12 @@ export type UserType = {
 }
 
 const initialPeople: UserType[] = [
-    // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
-    {_id: 0, name: 'Кот Матроскин', age: 3},
-    {_id: 1, name: 'Александр Розенбаум', age: 66},
-    {_id: 2, name: 'Коля Герасимов', age: 16},
-    {_id: 3, name: 'Виктор Цой', age: 44},
-    {_id: 4, name: 'Дмитрий Нагиев', age: 40},
-    {_id: 5, name: 'Ирина Салтыкова', age: 55},
+    {_id: 0, name: 'Cat Arnold', age: 3},
+    {_id: 1, name: 'Joe Biden', age: 66},
+    {_id: 2, name: 'Gretta Tunberg', age: 16},
+    {_id: 3, name: 'Justin Timberlake', age: 44},
+    {_id: 4, name: 'Alice Cooper', age: 40},
+    {_id: 5, name: 'Moby', age: 55},
 ]
 
 const HW8 = () => {
@@ -36,26 +29,26 @@ const HW8 = () => {
     const sortUp = () => {
         setPeople(
             homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
-        ) // в алфавитном порядке a.name > b.name
+        ) // a.name > b.name
         setCurrentSort('up')
     }
 
     const sortDown = () => {
         setPeople(
             homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
-        ) // в обратном порядке a.name < b.name}
+        ) // a.name < b.name}
         setCurrentSort('down')
     }
     const check18 = () => {
         setPeople(
             homeWorkReducer(initialPeople, {type: 'check', payload: 18})
-        ) // совершеннолетние
+        )
         setCurrentSort('18')
     }
 
     return (
         <div id={'hw3'}>
-            <div className={s2.hwTitle}>Homework #8</div>
+            <div className={s2.hwTitle}>Super sort</div>
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.buttonsContainer}>
